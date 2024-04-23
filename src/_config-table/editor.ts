6d01e313c72bcd3f29690ae257915af01d5d7e1b
huangqiuyun03 <huangqiuyun03@kuaishou.com>
2024-04-23 21:27:26 +0800

    feat(config-table):add configs

--- src/_config-table/editor.ts
+++ src/_config-table/editor.ts
@@ -1,20 +1,73 @@
+import { filter } from "lodash";
+
 export default {
-  "@init"({ setDesc }) {
-    setDesc("未选择组件");
-  },
+  // "@init"({ setDesc }) {
+  //   setDesc("未选择组件");
+  // },
   ":root": [
     {
-      title: "选择表单",
-      type: "sceneComSelector",
+      title: '选择组件',
+      type: 'sceneComSelector',
+      options: {
+        filter:({namespace}) => {
+          return namespace === 'mybricks.normal-pc.table'
+        }
+      },
       value: {
         get({ data }) {
-          return data?.table ?? {};
+          return data.comDef
         },
-        set({ data, setDesc }, table) {
-          data.table = table;
-          setDesc(table.title);
-        },
-      },
+        set({ data, setDesc }, comDef) {
+          data.comDef = comDef
+          setDesc(comDef.title)
+        }
+      }
     },
+    // {
+    //   title: "列配置",
+    //   type: "switch",
+    //   description: "开关开启后，增加对应列配置",
+    //   value: {
+    //     get({ data }) {
+    //       return data.isEditable || true;
+    //     },
+    //     set({ data, setDesc, input }, value: boolean) {
+    //       data.isEditable = value;
+    //     },
+    //   }
+    // },
+    {
+      title: '列操作',
+      type: 'Select',
+      // ifVisible({ data }) {
+      //   return data.isEditable;
+      // },
+      options: [
+        { label: '设置列', value: 'setColumn' },
+        { label: '列前添加列', value: 'addBeforeColumn' },
+        { label: '列后添加列', value: 'addAfterColumn' },
+      ],
+      value: {
+        get({ data }) {
+          return data?.operationType || 'setColumn';
+        },
+        set({ data, input }, value: 'setColumn' | 'addBeforeColumn' | 'addAfterColumn') {
+          data.operationType = value;
+        }
+      }
+    },
+    // {
+    //   title: "分页配置",
+    //   type: "switch",
+    //   description: "开关开启后，增加对应列配置",
+    //   value: {
+    //     get({ data }) {
+    //       return data.isPagination || false;
+    //     },
+    //     set({ data, setDesc, input }, value: boolean) {
+    //       data.isPagination = value;
+    //     },
+    //   }
+    // },
   ],
 };
