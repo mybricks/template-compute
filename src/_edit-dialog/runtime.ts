02c73875ac41e5a44a4f19084b1f5c7bbd45e376
wufan12 <wufan12@kuaishou.com>
2024-04-26 15:55:10 +0800

    feat: 静态配置对话框&输入项调整
    fix: 默认值

--- src/_edit-dialog/runtime.ts
+++ src/_edit-dialog/runtime.ts
@@ -5,6 +5,8 @@ export interface Data {
   title?: string
   hideTitle: boolean
   comDef: any
+  comList?: Array<Record<string, any>>
+  useStaticData: boolean
   useFooter: boolean
   actionOptions: Array<{id: string, label: string, value: any, visible: boolean }>
 }
@@ -28,15 +30,23 @@ export default function ({ env, data, inputs, outputs, onError }: { env: any,  o
       console.log('comForm', comForm, data)
       let newItems = []
       if(formData) {
+        const slot = comForm.slots.find(({ id }) => id === "body");
         formData.forEach(item => {
-          const slot = comForm.slots.find(({ id }) => id === "body");
           let comItem = slot.appendChild({
             namespace: item.namespace || 'mybricks.normal-pc.form-text',
           })
           console.log('comItem', comItem, item)
         })
       }
-      const { title, hideTitle, actionOptions = [], useFooter } = data
+      const { title, hideTitle, actionOptions = [], useFooter, comList, useStaticData = false  } = data
+      if(!formData && useStaticData && comList?.length) {
+        const slot = comForm.slots.find(({ id }) => id === "body");
+        comList.forEach(item => {
+          let comItem = slot.appendChild({
+            namespace: item.namespace || 'mybricks.normal-pc.form-text',
+          })
+        })
+      }
       actionOptions.filter(i => i.visible).forEach(btn => {
         const defaultBtn = {
           title: btn.label,
@@ -57,11 +67,6 @@ export default function ({ env, data, inputs, outputs, onError }: { env: any,  o
         comForm.data.footerBtns.push(defaultBtn)
 
       })
-      // TODO: 表单配置字段、表单namespace字段
-      // if(formItems.some(ite => !ite.namesapce)) {
-      //   onError("组件namespace必填");
-      //   return
-      // }
 
       // 修改模板页面中的对话框数据
       comForm.data = merge(comForm.data, { title, hideTitle, actionOptions, useFooter })
